692a05216c34579cd7507598466e041a144822fa
anotherrandomuser1234 <mark_grech@hotmail.com>
2023-02-26 21:17:43 +0100

    WIP updating home page

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://decagi:<password>@cluster0.rqwictr.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
